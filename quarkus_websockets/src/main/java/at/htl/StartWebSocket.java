4cc52470b71a0a89c0c5f8f95c9f85580eead6a4
Jan Kaufmann <jan.kaufmann@liwest.at>
2021-11-26 12:37:20 +0100

    implement StartWebSocket class

---
+++ quarkus_websockets/src/main/java/at/htl/StartWebSocket.java
@@ -0,0 +1,59 @@
+package at.htl;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.EncodeException;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static java.util.Objects.requireNonNull;
+
+@ServerEndpoint("/chat/{username}")
+@ApplicationScoped
+public class StartWebSocket {
+    Map<String, Session> sessions = new ConcurrentHashMap<>();
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) {
+        sessions.put(username, session);
+    }
+
+    @OnClose
+    public void onClose(Session session, @PathParam("username") String username) {
+        sessions.remove(username);
+        broadcast("User " + username + " has left");
+    }
+
+    @OnError
+    public void onError(Session session, @PathParam("username") String username, Throwable throwable) {
+        sessions.remove(username);
+        broadcast("User " + username + " has left on error: " + throwable);
+    }
+
+    @OnMessage
+    public void onMessage(String message, @PathParam("username") String username) {
+        if (message.equalsIgnoreCase("_ready_")) {
+            broadcast("User " + username + " joined");
+        }
+        else {
+            broadcast(">>" + username + ": " + message);
+        }
+    }
+
+    private void broadcast(String message) {
+        sessions.values().forEach(s -> {
+            s.getAsyncRemote().sendObject(message, result -> {
+                if (result.getException() != null) {
+                    System.out.println("Unable to send message; " + result.getException());
+                }
+            });
+        });
+    }
+}
